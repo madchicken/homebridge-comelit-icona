e53a9ebc54ae263669b6201da450cc55070d781f
Pierpaolo Follia <pfollia@gmail.com>
2022-04-21 22:46:49 +0200

    add try catch

--- src/icona-platform.ts
+++ src/icona-platform.ts
@@ -38,58 +38,63 @@ export class IconaPlatform implements DynamicPlatformPlugin {
       this.log.info('Icona platform: discovering devices...');
       const client = new IconaBridgeClient(config.bridge_url, config.bridge_port, this.log);
       await client.connect();
-      const code = await client.authenticate(config.icona_token);
-      if (code === 200) {
-        const addressBookAll = await client.getConfig('all');
-        if (addressBookAll) {
-          this.log.info(
-            'Available doors: %o',
-            addressBookAll.vip['user-parameters']['opendoor-address-book']
-          );
-          addressBookAll.vip['user-parameters']['opendoor-address-book'].forEach(item => {
-            // generate a unique id for the accessory this should be generated from
-            // something globally unique, but constant, for example, the device serial
-            // number or MAC address
-            const uuid = this.api.hap.uuid.generate(
-              `${item.name}-${item['apt-address']}:${item['output-index']}`
+      try {
+        const code = await client.authenticate(config.icona_token);
+        if (code === 200) {
+          const addressBookAll = await client.getConfig('all');
+          if (addressBookAll) {
+            this.log.info(
+              'Available doors: %o',
+              addressBookAll.vip['user-parameters']['opendoor-address-book']
             );
-
-            // see if an accessory with the same uuid has already been registered and restored from
-            // the cached devices we stored in the `configureAccessory` method above
-            const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-            if (existingAccessory) {
-              // the accessory already exists
-              this.log.info(
-                'Restoring existing accessory from cache:',
-                existingAccessory.displayName
+            addressBookAll.vip['user-parameters']['opendoor-address-book'].forEach(item => {
+              // generate a unique id for the accessory this should be generated from
+              // something globally unique, but constant, for example, the device serial
+              // number or MAC address
+              const uuid = this.api.hap.uuid.generate(
+                `${item.name}-${item['apt-address']}:${item['output-index']}`
               );
-              new DoorAccessory(this, existingAccessory, config, this.log);
-            } else {
-              // the accessory does not yet exist, so we need to create it
-              this.log.info('Adding new accessory:', item.name);
-              // create a new accessory
-              const accessory = new this.api.platformAccessory(
-                capitalize(item.name.toLocaleLowerCase()),
-                uuid,
-                Categories.DOOR
-              );
-              // Store icona config data in context
-              accessory.context.addressBookAll = addressBookAll;
-              accessory.context.doorItem = item;
-              new DoorAccessory(this, accessory, config, this.log);
-              this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-            }
-          });
+
+              // see if an accessory with the same uuid has already been registered and restored from
+              // the cached devices we stored in the `configureAccessory` method above
+              const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+
+              if (existingAccessory) {
+                // the accessory already exists
+                this.log.info(
+                  'Restoring existing accessory from cache:',
+                  existingAccessory.displayName
+                );
+                new DoorAccessory(this, existingAccessory, config, this.log);
+              } else {
+                // the accessory does not yet exist, so we need to create it
+                this.log.info('Adding new accessory:', item.name);
+                // create a new accessory
+                const accessory = new this.api.platformAccessory(
+                  capitalize(item.name.toLocaleLowerCase()),
+                  uuid,
+                  Categories.DOOR
+                );
+                // Store icona config data in context
+                accessory.context.addressBookAll = addressBookAll;
+                accessory.context.doorItem = item;
+                new DoorAccessory(this, accessory, config, this.log);
+                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+              }
+            });
+          } else {
+            this.log.error(`No configuration received from ICONA client. Shutting down.`);
+          }
         } else {
-          this.log.error(`No configuration received from ICONA client. Shutting down.`);
+          this.log.error(
+            `Received code ${code} from ICONA client during authentication phase. Shutting down.`
+          );
         }
-      } else {
-        this.log.error(
-          `Received code ${code} from ICONA client during authentication phase. Shutting down.`
-        );
+      } catch (e) {
+        this.log.error(`Error during authentication phase. Shutting down.`, e);
+      } finally {
+        await client?.shutdown();
       }
-      await client.shutdown();
     };
   }
 
